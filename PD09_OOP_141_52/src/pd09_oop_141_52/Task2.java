118a8fdcc7e09fc7b18b27f642eb65aa5b414fc7
hkb2001 <hasnainkbhatti@gmail.com>
2021-04-24 15:11:21 +0500

    updated Task2

--- PD09_OOP_141_52/src/pd09_oop_141_52/Task2.java
+++ PD09_OOP_141_52/src/pd09_oop_141_52/Task2.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package pd09_oop_141_52;
+
 import java.util.Scanner;
 
 /**
@@ -15,27 +16,36 @@ public class Task2 {
     /**
      * @param args the command line arguments
      */
-        private static Scanner input = new Scanner(System.in);
+    private static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
         // TODO code application logic here
         String s;
         System.out.println("Enter the string: ");
         s = input.nextLine();
-        String ver=displayVertical(s);
+        String ver = displayVertical(s);
     }
 
     public static String displayVertical(String s) {
         int count = 0;
-        String ver=null;
+        String ver = null;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z' || s.charAt(i) >= 'A'
-                    && s.charAt(i) <= 'Z') {
+                    && s.charAt(i) <= 'Z' || s.charAt(i) >= '0' && s.charAt(i) <= '9'
+                    || s.charAt(i) == '.' || s.charAt(i) == '?' || s.charAt(i) == '"' 
+                    || s.charAt(i) == '"' || s.charAt(i) == '\''
+                    || s.charAt(i) == ',' || s.charAt(i) == '-' || s.charAt(i) == '_' 
+                    || s.charAt(i) == '!' || s.charAt(i) == ':'
+                    || s.charAt(i) == ';' || s.charAt(i) == '(' || s.charAt(i) == ')' 
+                    || s.charAt(i) == '[' || s.charAt(i) == ']'
+                    || s.charAt(i) == '\\') {
                 System.out.println(s.charAt(i));
-                ver=s;
+                ver = s;
                 count++;
+
             }
+
         }
         return ver;
     }
-
 }
