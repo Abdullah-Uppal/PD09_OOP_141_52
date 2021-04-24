655a0ee44026284f982b52edd92435f318f3914a
hkb2001 <hasnainkbhatti@gmail.com>
2021-04-24 14:54:22 +0500

    task2 completed

---
+++ PD09_OOP_141_52/src/pd09_oop_141_52/Task2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pd09_oop_141_52;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Hp
+ */
+public class Task2 {
+
+    /**
+     * @param args the command line arguments
+     */
+        private static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s;
+        System.out.println("Enter the string: ");
+        s = input.nextLine();
+        String ver=displayVertical(s);
+    }
+
+    public static String displayVertical(String s) {
+        int count = 0;
+        String ver=null;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z' || s.charAt(i) >= 'A'
+                    && s.charAt(i) <= 'Z') {
+                System.out.println(s.charAt(i));
+                ver=s;
+                count++;
+            }
+        }
+        return ver;
+    }
+
+}
